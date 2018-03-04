d9a97320412b194b34bcc931a83a1161ac0eee45
Holeil <holeilaccougm@gmail.com>
2018-03-04 18:53:03 +0300

    Commit 3

--- src/index.js
+++ src/index.js
@@ -9,7 +9,11 @@ module.exports = function longestConsecutiveLength(array) {
   let count = 0;
 
   for(let i = 0; i < array.length; i++) {
-    count++;
+    if(count == 0) count++;
+    if(array[i+1] == array[i]+1) {
+      count++;
+    }
+    else if(array[i+1] == array[i]) continue;
     if(array[i+1] != array[i]+1) {
       if(count > length) length = count;
       count = 0;
